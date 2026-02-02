1f3c1c57fd33909c286abc83741293a3cf1ed54c
Leonardo (YennerChip) <ronsat654@gmail.com>
2026-02-02 17:11:41 -0500

    Refactoring and better documentation for funcitons

---
+++ include/core/systems/gkc_ecs_event_system.h
@@ -0,0 +1,8 @@
+//
+// Created by ronsa on 1/30/2026.
+//
+
+#ifndef MOO_GKC_ECS_EVENT_SYSTEM_H
+#define MOO_GKC_ECS_EVENT_SYSTEM_H
+
+#endif //MOO_GKC_ECS_EVENT_SYSTEM_H
---
+++ src/core/systems/gkc_ecs_event_system.cpp
@@ -0,0 +1,3 @@
+//
+// Created by ronsa on 1/30/2026.
+//
